a0f04566d7410bf30e125afc632153ed7fc7610e
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2023-04-18 23:14:55 +0700

    added sprite loading. added player initial position

--- lib/game.dart
+++ lib/game.dart
@@ -6,6 +6,12 @@ import 'objects/snake.dart';
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
 
+  static final Vector2 playerInitialPosition = Vector2
+    (
+      (6*32),
+      (8*32)
+  );
+
   @override
   State<Game> createState() => _GameState();
 }
@@ -17,31 +23,31 @@ class _GameState extends State<Game> {
       map: WorldMapByTiled(
           "map/map.json",
           forceTileSize: Vector2(32,32),
-          objectsBuilder: {
-            "ghost_red": (properties) => Game.redGhost,
-            "ghost_yellow": (properties) => Game.yellowGhost,
-            "pixel": (properties) => Pixel.withSprite(properties.position, properties.size),
-            "big_pixel":(properties) => BigPixel.withSprite(properties.position, properties.size),
-            "fruit":(properties) => Fruit.withSprite(properties.position, properties.size),
-          }
+          // objectsBuilder: {
+          //   "ghost_red": (properties) => Game.redGhost,
+          //   "ghost_yellow": (properties) => Game.yellowGhost,
+          //   "pixel": (properties) => Pixel.withSprite(properties.position, properties.size),
+          //   "big_pixel":(properties) => BigPixel.withSprite(properties.position, properties.size),
+          //   "fruit":(properties) => Fruit.withSprite(properties.position, properties.size),
+          // }
       ),
       player: Snake(Game.playerInitialPosition, Vector2(32,32)),
-      overlayBuilderMap: {
-        MyGameInterface.overlayKey : (context, game)
-        {
-          return MyGameInterface(game: game);
-        }
-      },
-      initialActiveOverlays: const
-      [
-        MyGameInterface.overlayKey
-      ],
-      components: [
-        MyGameController()
-      ],
-      cameraConfig: CameraConfig(
-          moveOnlyMapArea: true
-      ),
+      // overlayBuilderMap: {
+      //   MyGameInterface.overlayKey : (context, game)
+      //   {
+      //     return MyGameInterface(game: game);
+      //   }
+      // },
+      // initialActiveOverlays: const
+      // [
+      //   MyGameInterface.overlayKey
+      // ],
+      // components: [
+      //   MyGameController()
+      // ],
+      // cameraConfig: CameraConfig(
+      //     moveOnlyMapArea: true
+      // ),
     );
 
   }
--- lib/resources/sprite_sheet.dart
+++ lib/resources/sprite_sheet.dart
@@ -1,13 +1,15 @@
 import 'package:bonfire/bonfire.dart';
 
 class GameSprites {
-  // static Future<Sprite> get bigPixelSprite =>
-  //     Sprite.load("pacman_spritesheet.png",
-  //         srcPosition: Vector2(8, 9 * 32 + 8), srcSize: Vector2(16, 16));
-  //
-  // static Future<Sprite> get pixelSprite => Sprite.load("pacman_spritesheet.png",
-  //     srcPosition: Vector2(32, 9 * 32), srcSize: Vector2(32, 32));
-
   static Future<Sprite> get apple => Sprite.load("fruit/apple.png",
+      srcPosition: Vector2(0, 0), srcSize: Vector2(40, 40));
+
+  static Future<Sprite> get snakeHeadDown => Sprite.load("snake/head_down.png",
+      srcPosition: Vector2(0, 0), srcSize: Vector2(40, 40));
+
+  static Future<Sprite> get snakeBodyVertical => Sprite.load("snake/body_vertical.png",
+      srcPosition: Vector2(0, 0), srcSize: Vector2(40, 40));
+
+  static Future<Sprite> get snakeBodyTopLeft => Sprite.load("snake/body_topleft.png",
       srcPosition: Vector2(0, 0), srcSize: Vector2(40, 40));
 }
